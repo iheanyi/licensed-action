71834775ed097d5e8eff83a57d03ea8f0f851508
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2019-02-15 12:37:26 -0500

    Add in scaffolding for pushing up new licenses, add some messages.

--- check-licenses.sh
+++ check-licenses.sh
@@ -2,10 +2,26 @@
 set -e
 bundle install
 
+push_new_licenses () {
+  git status
+  if [ -n "$(git status --porcelain .licenses)" ]; then
+    echo "New licenses found, pushing to repo..."
+    # TODO: Add git add and git pushing logic.
+    # git add .licenses/
+    echo "Finish pushing license cache to repo."
+  else
+    echo "No new licenses found."
+  fi
+}
+
+echo "Checking open-source licenses..."
 if [[ -z "$CONFIG_PATH" ]]; then
   bundle exec licensed cache
+  push_new_licenses
   bundle exec licensed status
 else
   bundle exec licensed cache -c $CONFIG_PATH
+  push_new_licenses
   bundle exec licensed status -c $CONFIG_PATH
 fi
+echo "Finished checking licenses, all clear!"
