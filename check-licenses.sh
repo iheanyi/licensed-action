a7ebe3c6361cd126a74f3dbd5b14ac57850efa92
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2019-02-15 13:39:35 -0500

    Script cleanup.

--- check-licenses.sh
+++ check-licenses.sh
@@ -5,11 +5,7 @@ bundle install
 setup_git_env () {
   echo "Setting up git environment..."
   git config --global user.name "GitHub Actions"
-  # TODO: Figure out how to perhaps set this to a more sane value.
-  git config --global user.email "test@example.com"
-
-  # git fetch
-  # git pull
+  git config --global user.email "hubot@github.com"
 }
 
 push_new_licenses () {
@@ -17,7 +13,6 @@ push_new_licenses () {
   REPO_URL="https://$GITHUB_TOKEN:x-oauth-basic@github.com/$GITHUB_REPOSITORY.git"
   if [ -n "$(git status --porcelain .licenses)" ]; then
     echo "New licenses found, pushing to repo..."
-    # TODO: Add git add and git pushing logic.
     git add .licenses/
     git commit -m "Update licenses cache."
     git push --set-upstream origin $(git symbolic-ref --short HEAD)
